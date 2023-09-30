f60f363d5df5d2d1876a6049ca57f5bdb250118c
Huzaifa-Yaqoob <51huzaifayaqoob@gmail.com>
2023-09-30 18:10:07 +0500

    Added logo in the background of about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Metadata } from "next";
 import About from "@/components/about/About";
 import OurMission from "@/components/about/OurMission";
@@ -13,7 +14,10 @@ export const metadata: Metadata = {
 
 export default function AboutPage() {
   return (
-    <div className="my-container flex flex-col gap-4">
+    <div className="my-container flex flex-col gap-4 ">
+      <div className="fixed top-1/2 left-2/4 z-[-1] w-20 h-20">
+        <Image src="/logo.svg" alt="Logo" fill className="opacity-10" />
+      </div>
       <About />
       <OurStory />
       <OurMission />
